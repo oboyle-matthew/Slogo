aeba621c3730c9e589095c74af6817254c798778
Matthew O'Boyle <mo121@duke.edu>
2017-10-21 12:45:11 -0400

    Run button reads text

--- src/GUI/TextInputBox.java
+++ src/GUI/TextInputBox.java
@@ -1,7 +1,10 @@
 package GUI;
 
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -27,13 +30,16 @@ public class TextInputBox extends HBox{
 		clearButton = new Button ("clear");
 		runButton.setPrefWidth(50);
 		clearButton.setPrefWidth(50);
-		runButton.setStyle("-fx-font-family: Andale Mono");
-		runButton.setStyle("-fx-padding: 6");
-		runButton.setStyle("-fx-font-size: 1em");
-		runButton.setTextFill(Color.WHITE);
-		runButton.setStyle("-fx-background-color: #001A57;");
-		runButton.setStyle("-fx-effect: dropshadow(gaussian, rgba(67,96,156,0.25) , 0,0,2,2 )");
-		clearButton.setStyle("-fx-background-color: DARKBLUE");
+		createRunButton();
+//    	Old way of writing this below
+//    	EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() {
+//	        @Override public void handle(MouseEvent e) {
+//	        	begin(gametype);}
+//        };
+		EventHandler<? super MouseEvent> eventHandler =  e->readText("test");
+		runButton.addEventHandler(MouseEvent.MOUSE_CLICKED, eventHandler);
+    	
+		clearButton.setStyle("-fx-background-color: RED");
 		ButtonBlock = new VBox();
 		ButtonBlock.getChildren().add(runButton);
 		ButtonBlock.getChildren().add(clearButton);
@@ -44,5 +50,20 @@ public class TextInputBox extends HBox{
 		this.setLayoutX(30);
 		this.setLayoutY(450);
 		}
+
+	private void readText(String test) {
+		String tester = commandField.getText();
+		commandField.clear();
+		System.out.println(tester);
+	}
+	
+	private void createRunButton() {
+		runButton.setStyle("-fx-font-family: Andale Mono");
+		runButton.setStyle("-fx-padding: 6");
+		runButton.setStyle("-fx-font-size: 1em");
+		runButton.setTextFill(Color.WHITE);
+		runButton.setStyle("-fx-background-color: #001A57;");
+		runButton.setStyle("-fx-effect: dropshadow(gaussian, rgba(67,96,156,0.25) , 0,0,2,2 )");
+	}
 
 }

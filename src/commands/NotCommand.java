c2d779a957afbe53d148458e8bf3e03657d32538
Walker Willetts <wlkrwilletts@gmail.com>
2017-10-22 14:12:30 -0400

    Fixed warning in NotCommand class

--- src/commands/NotCommand.java
+++ src/commands/NotCommand.java
@@ -10,11 +10,11 @@ import modelLogic.Turtle;
  */
 public class NotCommand implements ExecutableCommand {
 
-	private static final double DEFAULT_VALUE = 15;
+	private static final double DEFAULT_VALUE = 0;
 	
 	@Override
 	public double execute(Turtle tortuga, List<Double> args) {
-		if(args == null || args.size() < 1) return ( DEFAULT_VALUE == 0 ? 1 : 0);
+		if(args == null || args.size() < 1) return DEFAULT_VALUE;
 		return ( args.remove(0) == 0 ? 1 : 0);
 	}
 	

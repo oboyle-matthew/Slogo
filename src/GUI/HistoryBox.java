4499f66757623be5c3e01b382d9151d68e2be508
Matthew O'Boyle <mo121@duke.edu>
2017-10-23 11:26:52 -0400

    History pane

--- src/GUI/HistoryBox.java
+++ src/GUI/HistoryBox.java
@@ -3,6 +3,7 @@ package GUI;
 import java.util.ArrayList;
 import java.util.List;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
@@ -17,21 +18,26 @@ public class HistoryBox extends HBox{
 //	private ListView<String> myHistoryPaneView;
 	private List<String> myHistory;
 	private ScrollPane myCommandHistoryPane;
+	private ListView<String> myHistoryPaneView;
 	
 	public HistoryBox() {
 		myCommandHistoryPane = new ScrollPane();
+		myHistoryPaneView = new ListView<String>();
+		myHistory = new ArrayList<String>();
+		myHistory.add("Command One");
+		myHistory.add("Command Two");
+		myHistory.add("Command Three");
+		myHistory.add("Command Four");
+		myHistory.add("Command Five");
+		ObservableList<String> items =FXCollections.observableArrayList(myHistory);
+        myHistoryPaneView.setItems(items);
+        myCommandHistoryPane.setContent(myHistoryPaneView);
 		this.getChildren().add(myCommandHistoryPane);
 		this.setLayoutX(500);
+		this.setLayoutY(30);
 //		this.getChildren().add(commandField);
 //		this.setLayoutX(30);
 //		this.setLayoutY(450);
-	}
-
-	private void createHistoryBox() {
-//		this.getChildren().add(commandField);
-		this.getChildren().add(ButtonBlock);
-		this.setLayoutX(500);
-		this.setLayoutY(50);
 	}
 
 }

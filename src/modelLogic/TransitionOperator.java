c6f9f4b5ad47394caac6224cfe2eed788a2a7687
BillGatesNephew <wlkrwilletts@gmail.com>
2017-10-30 17:14:25 -0400

    Added documentation to the TransitionOperator class

--- src/modelLogic/TransitionOperator.java
+++ src/modelLogic/TransitionOperator.java
@@ -1,6 +1,6 @@
 package modelLogic;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import javafx.animation.FadeTransition;
@@ -14,18 +14,25 @@ import javafx.util.Duration;
 
 public class TransitionOperator {
 	
+	/* Final Variables */ 
 	private static final double ROTATION_SPEED = 0.2 * 1000; 
 	private static final double MOVEMENT_SPEED = 0.1 * 1000;
 	private static final double FADE_SPEED = 0.3 * 1000;
-
+	
+	/* Instance Variables */
 	private List<Transition> waitingTransitions;
 	private boolean transitionRunning;
 	
 	TransitionOperator() {
-		waitingTransitions = new ArrayList<>();
+		waitingTransitions = new LinkedList<>();
 		transitionRunning = false; 
 	}
 	
+	/**
+	 * Either add a transition to the list of waiting transitions, or play the transition
+	 * if nothing is playing or in the list 
+	 * @param a is a {@code Transition} that is waiting to be run 
+	 */
 	private void addAndPlay(Transition a) {
 		if(!transitionRunning && waitingTransitions.size() == 0) {
 			transitionRunning = true;	
@@ -35,6 +42,9 @@ public class TransitionOperator {
 		}
 	}
 	
+	/**
+	 * Plays the next transition waiting to run if there are any
+	 */
 	private void transitionFinished() {
 		transitionRunning = false;
 		if(waitingTransitions.size() != 0) {
@@ -44,6 +54,24 @@ public class TransitionOperator {
 		}
 	}
 	
+	/**
+	 * Creates a FadeIn Transition and adds it to the list of transitions waiting to run 
+	 * @param obj is a {@code Node} on which the FadeIn transition will run
+	 */
+	public void createFadeIn(Node obj) {
+		FadeTransition ft = new FadeTransition(Duration.millis(FADE_SPEED));
+		ft.setCycleCount(1);
+		ft.setNode(obj);
+		ft.setDuration(Duration.millis(FADE_SPEED));
+		ft.setToValue(1);
+		ft.setOnFinished(e -> transitionFinished());
+		addAndPlay(ft);
+	}
+
+	/**
+	 * Creates a FadeOut Transition and adds it to the list of transitions waiting to run 
+	 * @param obj is a {@code Node} on which the FadeOut transition will run
+	 */
 	public void createFadeOut(Node obj) {
 		FadeTransition ft = new FadeTransition(Duration.millis(FADE_SPEED));
 		ft.setCycleCount(1);
@@ -54,27 +82,10 @@ public class TransitionOperator {
 		addAndPlay(ft);
 	}
 	
-	public void createFadeIn(Node obj) {
-		FadeTransition ft = new FadeTransition(Duration.millis(FADE_SPEED));
-		ft.setCycleCount(1);
-		ft.setNode(obj);
-		ft.setDuration(Duration.millis(FADE_SPEED));
-		ft.setToValue(1);
-		ft.setOnFinished(e -> transitionFinished());
-		addAndPlay(ft);
-	}
-	
-	
-	public void createRotation(Node obj, double angle) {
-		RotateTransition rt = new RotateTransition();
-		rt.setByAngle(angle);
-		rt.setCycleCount(1);
-		rt.setNode(obj);
-		rt.setDuration(Duration.millis(ROTATION_SPEED));
-		rt.setOnFinished(e-> transitionFinished());
-		addAndPlay(rt);
-	}
-	
+	/**
+	 * Creates a PathTransition and adds it to the list of transitions waiting to run 
+	 * @param obj is a {@code Node} on which the PathTransition will run
+	 */
 	public void createMovement(Node obj, Path movementPath, double x, double y) {
 		PathTransition pt = new PathTransition();
 		pt.setPath(movementPath);
@@ -93,5 +104,19 @@ public class TransitionOperator {
 			transitionFinished(); 
 		});
 		addAndPlay(pt);
+	}
+	
+	/**
+	 * Creates a RotateTransition and adds it to the list of transitions waiting to run 
+	 * @param obj is a {@code Node} on which the RotateTransition will run
+	 */
+	public void createRotation(Node obj, double angle) {
+		RotateTransition rt = new RotateTransition();
+		rt.setByAngle(angle);
+		rt.setCycleCount(1);
+		rt.setNode(obj);
+		rt.setDuration(Duration.millis(ROTATION_SPEED));
+		rt.setOnFinished(e-> transitionFinished());
+		addAndPlay(rt);
 	}
 }
